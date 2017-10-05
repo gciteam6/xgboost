e8383384ad69b047aa13271d47674d356da4e735
nyuge <aiben.mail1@gmail.com>
2017-10-05 19:12:35 +0900

    Refine DataFrameHandlerbase
    + Add to_tsv function
    + Add file saving supports

--- src/data/base.py
+++ src/data/base.py
@@ -1,16 +1,23 @@
 # Built-in modules
 from copy import deepcopy
 from os import pardir, path, makedirs
+# Third-party modules
+import pandas as pd
 
 
 PROJECT_ROOT_PATH = path.join(path.dirname(__file__), pardir, pardir)
 RAW_DATA_BASEPATH = path.join(PROJECT_ROOT_PATH, "data/raw")
 INTERIM_DATA_BASEPATH = path.join(PROJECT_ROOT_PATH, "data/interim")
 PROCESSED_DATA_BASEPATH = path.join(PROJECT_ROOT_PATH, "data/processed")
+
+DATETIME_FORMAT = "(?P<year>\d{4})(?P<month>\d{1,2})(?P<day>\d{1,2})(?P<hour>\d{2})(?P<minute>\d{2})"
 KWARGS_READ_CSV_BASE = {
     "sep": "\t",
     "header": 0,
     "na_values": ['', '　']
+}
+KWARGS_TO_CSV_BASE = {
+    "sep": "\t"
 }
 
 
@@ -34,12 +41,25 @@ class DataFrameHandlerBase(PathHandlerBase):
     def __init__(self):
         super().__init__()
         self.KWARGS_READ_CSV_BASE = KWARGS_READ_CSV_BASE
+        self.KWARGS_TO_CSV_BASE = KWARGS_TO_CSV_BASE
+        self.DATETIME_FORMAT = DATETIME_FORMAT
 
     def gen_read_csv_kwargs(self, kwargs_to_add: dict):
         ret_dict = deepcopy(self.KWARGS_READ_CSV_BASE)
-        ret_dict.update(kwargs_to_add)
+        if kwargs_to_add is not None:
+            ret_dict.update(kwargs_to_add)
 
         return ret_dict
+
+    def gen_to_csv_kwargs(self, kwargs_to_add: dict):
+        ret_dict = deepcopy(self.KWARGS_TO_CSV_BASE)
+        if kwargs_to_add is not None:
+            ret_dict.update(kwargs_to_add)
+
+        return ret_dict
+
+    def parse_datetime(self, df):
+        return pd.to_datetime(df.str.extract(self.DATETIME_FORMAT, expand=False))
 
 
 if __name__ == '__main__':
