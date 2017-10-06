65185a3a279b242bfb0e4845964b245654684d36
nyuge <aiben.mail1@gmail.com>
2017-10-06 10:10:32 +0900

    Refine AmedasHandler
    - segregate util. functions as a method in a base class
    - rename the fuction name to the one for general purposes

--- src/data/amedas.py
+++ src/data/amedas.py
@@ -2,83 +2,43 @@ from glob import glob
 # Third-party modules
 import pandas as pd
 # Hand-made modules
-from base import DataFrameHandlerBase
+from base import LocationHandlerBase
 
-
+AMD_REGEX_DIRNAME = "amd[1-5]"
 KWARGS_READ_CSV_AMD_MASTER = {
     "index_col": 0,
 }
 KWARGS_READ_CSV_AMD_LOG = {
     "index_col": 0,
     "na_values": ['', '　']
-    # "parse_dates": ["datetime"],
-    # "date_parser": lambda yyyymmddHHMM: pd.to_datetime(yyyymmddHHMM, format="%Y%m%d%H%M", errors="coerce"),
 }
 
-LATLNGALT_UKISHIMA = (35.517558, 139.786920, 4.7)
-LATLNGALT_OUGIZIMA = (35.488680, 139.727451, 4.8)
-LATLNGALT_YONEKURAYAMA = (35.583302, 138.573118, 366.9)
-LABEL_LAT_HOUR, LABEL_LAT_MINUTE = "lat1", "lat2"
-LABEL_LNG_HOUR, LABEL_LNG_MINUTE = "lng1", "lng2"
-LABEL_LAT_DECIMAL, LABEL_LNG_DECIMAL = "lat_dec", "lng_dec"
 
-
-class AmedasPoint(DataFrameHandlerBase):
-    def __init__(self, amd_master_filepath):
-        super().__init__()
-        self.LATLNGALT_UKISHIMA = LATLNGALT_UKISHIMA
-        self.LATLNGALT_OUGIZIMA = LATLNGALT_OUGIZIMA
-        self.LATLNGALT_YONEKURAYAMA = LATLNGALT_YONEKURAYAMA
-
-        self.df_amedas_point = pd.read_csv(
-            amd_master_filepath, **self.gen_read_csv_kwargs(KWARGS_READ_CSV_AMD_MASTER)
-        )
-        self.df_amedas_point[LABEL_LAT_DECIMAL] = self.df_amedas_point.apply(
-            lambda df: self.cast_60_to_10(df[LABEL_LAT_HOUR], df[LABEL_LAT_MINUTE]), axis=1
-        )
-        self.df_amedas_point[LABEL_LNG_DECIMAL] = self.df_amedas_point.apply(
-            lambda df: self.cast_60_to_10(df[LABEL_LNG_HOUR], df[LABEL_LNG_MINUTE]), axis=1
-        )
-
-    def get_near_amedas_points(self, lat_mid, lng_mid, half_grid_size):
-        lat_max, lat_min = lat_mid + half_grid_size, lat_mid - half_grid_size
-        lng_max, lng_min = lng_mid + half_grid_size, lng_mid - half_grid_size
-
-        lat_within_mesh = self.df_amedas_point[LABEL_LAT_DECIMAL].apply(
-            lambda lat: True if (lat_min <= lat <= lat_max) else False
-        )
-        lng_within_mesh = self.df_amedas_point[LABEL_LNG_DECIMAL].apply(
-            lambda lng: True if lng_min <= lng <= lng_max else False
-        )
-
-        flg_within_mesh = [is_lat and ls_lng for (is_lat, ls_lng) in zip(lat_within_mesh, lng_within_mesh)]
-
-        return self.df_amedas_point.loc[flg_within_mesh, :]
-
-    @staticmethod
-    def cast_60_to_10(hour, minute, second=0):
-        return hour + (minute / 60) + (second / 3600)
-
-
-class AmedasData(DataFrameHandlerBase):
-    def __init__(self, amd_file_prefix="amd_", amd_file_suffix=".tsv"):
-        super().__init__()
+class AmedasHandler(LocationHandlerBase):
+    def __init__(self,
+                 amd_master_filepath,
+                 amd_file_prefix="amd_",
+                 amd_file_suffix=".tsv"):
+        super().__init__(amd_master_filepath, **KWARGS_READ_CSV_AMD_MASTER)
         self.amd_file_prefix = amd_file_prefix
         self.amd_file_suffix = amd_file_suffix
-        self.amd_rexex_dirname = "amd[1-5]"
+        self.AMD_REGEX_DIRNAME = AMD_REGEX_DIRNAME
 
-    def read_amd_data(self, path_or_buf):
+    def read_tsv(self, path_or_buf):
         df_ret = pd.read_csv(path_or_buf, **self.gen_read_csv_kwargs(KWARGS_READ_CSV_AMD_LOG))
         df_ret.index = self.parse_datetime(pd.Series(df_ret.index).apply(str))
         return df_ret
+
+    def to_tsv(self, df, path_or_buf, **kwargs):
+        df.to_csv(path_or_buf, **self.gen_to_csv_kwargs(kwargs))
 
     def gen_filepath_list(self, aid_list):
         amd_regex_filepath_list = [
             self.path.join(
                 self.INTERIM_DATA_BASEPATH,
-                self.amd_rexex_dirname,
-                self.amd_file_prefix + str(aid)) + self.amd_file_suffix \
-            for aid in aid_list
+                self.AMD_REGEX_DIRNAME,
+                self.amd_file_prefix + str(aid) + self.amd_file_suffix
+            ) for aid in aid_list
         ]
 
         return [
@@ -87,17 +47,17 @@ class AmedasData(DataFrameHandlerBase):
             for amd_file in glob(amd_regex_filepath)
         ]
 
-    def retrive_amedas_data(self, amd_filepath_list, amd_name_list):
-        if len(amd_filepath_list) < 1:
+    def retrive_data(self, filepath_list, name_list):
+        if len(filepath_list) < 1:
             raise ValueError("Empty list ?")
 
-        df_ret = self.read_amd_data(amd_filepath_list[0])
-        df_ret.columns = [str(col_name) + '_' + amd_name_list[0] for col_name in df_ret.columns]
+        df_ret = self.read_tsv(filepath_list[0])
+        df_ret.columns = [str(col_name) + '_' + name_list[0] for col_name in df_ret.columns]
 
-        if len(amd_filepath_list) > 1:
-            for filepath, name in zip(amd_filepath_list[1:], amd_name_list[1:]):
+        if len(filepath_list) > 1:
+            for filepath, name in zip(filepath_list[1:], name_list[1:]):
                 df_ret = df_ret.merge(
-                    self.read_amd_data(filepath),
+                    self.read_tsv(filepath),
                     how="outer",
                     left_index=True,
                     right_index=True,
@@ -106,9 +66,6 @@ class AmedasData(DataFrameHandlerBase):
 
         return df_ret
 
-    def to_tsv(self, df, path_or_buf, **kwargs):
-        df.to_csv(path_or_buf, **self.gen_to_csv_kwargs(kwargs))
-
 
 if __name__ == '__main__':
-    print("concatenation!")
+    print("Amedas!")
--- src/data/base.py
+++ src/data/base.py
@@ -20,6 +20,13 @@ KWARGS_TO_CSV_BASE = {
     "sep": "\t"
 }
 
+LATLNGALT_UKISHIMA = (35.517558, 139.786920, 4.7)
+LATLNGALT_OUGIZIMA = (35.488680, 139.727451, 4.8)
+LATLNGALT_YONEKURAYAMA = (35.583302, 138.573118, 366.9)
+LABEL_LAT_HOUR, LABEL_LAT_MINUTE = "lat1", "lat2"
+LABEL_LNG_HOUR, LABEL_LNG_MINUTE = "lng1", "lng2"
+LABEL_LAT_DECIMAL, LABEL_LNG_DECIMAL = "lat_dec", "lng_dec"
+
 
 class PathHandlerBase(object):
     def __init__(self):
@@ -60,6 +67,43 @@ class DataFrameHandlerBase(PathHandlerBase):
 
     def parse_datetime(self, df):
         return pd.to_datetime(df.str.extract(self.DATETIME_FORMAT, expand=False))
+
+
+class LocationHandlerBase(DataFrameHandlerBase):
+    def __init__(self, master_filepath, **kwargs_location):
+        super().__init__()
+        self.LATLNGALT_UKISHIMA = LATLNGALT_UKISHIMA
+        self.LATLNGALT_OUGIZIMA = LATLNGALT_OUGIZIMA
+        self.LATLNGALT_YONEKURAYAMA = LATLNGALT_YONEKURAYAMA
+
+        self.location = pd.read_csv(
+            master_filepath, **self.gen_read_csv_kwargs(kwargs_location)
+        )
+        self.location[LABEL_LAT_DECIMAL] = self.location.apply(
+            lambda df: self.cast_60_to_10(df[LABEL_LAT_HOUR], df[LABEL_LAT_MINUTE]), axis=1
+        )
+        self.location[LABEL_LNG_DECIMAL] = self.location.apply(
+            lambda df: self.cast_60_to_10(df[LABEL_LNG_HOUR], df[LABEL_LNG_MINUTE]), axis=1
+        )
+
+    def get_near_observation_points(self, lat_mid, lng_mid, half_grid_size):
+        lat_max, lat_min = lat_mid + half_grid_size, lat_mid - half_grid_size
+        lng_max, lng_min = lng_mid + half_grid_size, lng_mid - half_grid_size
+
+        lat_within_mesh = self.location[LABEL_LAT_DECIMAL].apply(
+            lambda lat: True if (lat_min <= lat <= lat_max) else False
+        )
+        lng_within_mesh = self.location[LABEL_LNG_DECIMAL].apply(
+            lambda lng: True if lng_min <= lng <= lng_max else False
+        )
+
+        flg_within_mesh = [is_lat and ls_lng for (is_lat, ls_lng) in zip(lat_within_mesh, lng_within_mesh)]
+
+        return self.location.loc[flg_within_mesh, :]
+
+    @staticmethod
+    def cast_60_to_10(hour, minute, second=0):
+        return hour + (minute / 60) + (second / 3600)
 
 
 if __name__ == '__main__':
