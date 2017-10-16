0f799c9552cd18e2025e2b39edd9fbcd2a3cc9ba
nyuge <aiben.mail1@gmail.com>
2017-10-16 14:17:51 +0900

    Modify the code about train xgb models
    + add a function  "get_train_X_y"
    + specify the location by argument(must input) -> option(possible change)

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,39 +10,49 @@ sys.path.append(PROJECT_ROOT_DIRPATH)
 # Third-party modules
 import click
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 import bloscpack as bp
 # Hand-made modules
 from src.models.xgb import MyXGBRegressor
 
-TRAIN_FILEPATH_PREFIX = path.join(PROJECT_ROOT_DIRPATH, "data/interim/dataset.train")
-TRAIN_FILEPATH_SUFFIX = "#3"
+TRAIN_FILEPATH_PREFIX = path.join(PROJECT_ROOT_DIRPATH, "data/processed/dataset.train_X_y")
+TRAIN_FILEPATH_EXTENTION = "blp"
 XGB_PARAMS = {
-    "n_estimators": 300,
+    "n_estimators": 500,
     "nthread": -1,
     "seed": 1
 }
+LOCATIONS = (
+    "ukishima",
+    "ougishima",
+    "yonekurayama"
+)
 
 
 def get_train_X_y(train_filepath_prefix, train_filepath_suffix, fold_id=None):
-    func_gen_filepath = lambda file_attr: '.'.join([
-        train_filepath_prefix, file_attr, train_filepath_suffix
-    ])
-
-    train_X_y = bp.unpack_ndarray_file(func_gen_filepath("values"))
-
+    func_gen_filepath = lambda file_attr: '.'.join([train_filepath_prefix,
+                                                    file_attr,
+                                                    train_filepath_suffix])
+    values = bp.unpack_ndarray_file(func_gen_filepath("values"))
     if isinstance(fold_id, int):
-        extract_index = bp.unpack_ndarray_file(
-            func_gen_filepath("index.fold_{f}".format(f=fold_id))
+        whole_index = bp.unpack_ndarray_file(func_gen_filepath("index"))
+        columns = bp.unpack_ndarray_file(func_gen_filepath("columns"))
+        df_train = pd.DataFrame(values, index=pd.DatetimeIndex(whole_index), columns=columns)
+
+        removal_index = pd.DatetimeIndex(
+            bp.unpack_ndarray_file(func_gen_filepath("crossval{f}".format(f=fold_id)))
         )
-        return train_X_y[extract_index, :]
+        df_train.drop(removal_index, axis=0, inplace=True)
+
+        return df_train.values.astype(float)
     else:
-        return train_X_y
+        return values.astype(float)
 
 
 @click.command()
-@click.argument("location", type=str)
 @click.option("-t", "predict_target", flag_value="test", default=True)
-@click.option("-v", "predict_target", flag_value="cv")
+@click.option("-v", "predict_target", flag_value="crossval")
+@click.option("--location", "-l", type=str, default=None)
 @click.option("--fold-id", "-f", type=int)
 def main(location, predict_target, fold_id):
     logger = logging.getLogger(__name__)
@@ -54,28 +64,38 @@ def main(location, predict_target, fold_id):
     # TODO: -vがCLAで与えられたとき, -fの数で交差検証サブセットを取得する
     # TODO: python train_model.py -v -f 0 -> models/xgb/fit_model.fold0.location.pkl
 
-    m = MyXGBRegressor(model_name="{l}".format(l=location), params=XGB_PARAMS)
-
-    if predict_target == "test":
-        logger.info('#1: fit the model with all training dataset !')
-        train_X_y = get_train_X_y(TRAIN_FILEPATH_PREFIX, location + '.' + TRAIN_FILEPATH_SUFFIX)
-
-    elif predict_target == "cv":
-        if fold_id is None:
-            raise ValueError("Specify validation dataset number as an integer !")
-
-        logger.info('#1: fit the model without fold-id: {f} !'.format(f=fold_id))
-        train_X_y = get_train_X_y(TRAIN_FILEPATH_PREFIX, location + '.' + TRAIN_FILEPATH_SUFFIX, fold_id=fold_id)
-
+    if location is None:
+        location_list = LOCATIONS
     else:
-        raise ValueError("Invalid flag, '-t' or '-v' is permitted !")
+        location_list = [location, ]
 
-    logger.info('#1: get training dataset !')
+    for place in location_list:
+        if predict_target == "test":
+            logger.info('#1: fit the model with all training dataset @ {l} !'.format(l=place))
 
-    logger.info('#2: now modeling...')
-    m.fit(train_X_y[:, :-1], train_X_y[:, -1])
+            train_X_y = get_train_X_y(TRAIN_FILEPATH_PREFIX,
+                                      place + '.' + TRAIN_FILEPATH_EXTENTION,
+                                      fold_id=None)
+            m = MyXGBRegressor(model_name="test.{l}".format(l=place), params=XGB_PARAMS)
+        elif predict_target == "crossval":
+            if fold_id is None:
+                raise ValueError("Specify validation dataset number as an integer !")
 
-    logger.info('#2: model is pickled as a file !')
+            logger.info('#1: fit the model without fold-id: {f} @ {l} !'.format(f=fold_id, l=place))
+
+            train_X_y = get_train_X_y(TRAIN_FILEPATH_PREFIX,
+                                      place + '.' + TRAIN_FILEPATH_EXTENTION,
+                                      fold_id=fold_id)
+            m = MyXGBRegressor(model_name="crossval{i}.{l}".format(i=fold_id, l=place), params=XGB_PARAMS)
+        else:
+            raise ValueError("Invalid flag, '-t' or '-v' is permitted !")
+
+        logger.info('#1: get training dataset @ {l} !'.format(l=place))
+
+        logger.info('#2: now modeling...')
+        m.fit(train_X_y[:, :-1], train_X_y[:, -1])
+
+        logger.info('#2: model is pickled as a file @ {l} !'.format(l=place))
 
 
 if __name__ == '__main__':
