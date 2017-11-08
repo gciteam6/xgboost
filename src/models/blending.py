7c987595107af5de41684ecb5d8037f86b067a09
nyuge <aiben.mail1@gmail.com>
2017-11-08 18:14:23 +0900

    Enable to pickle a regression & preprocessors

--- src/models/blending.py
+++ src/models/blending.py
@@ -12,38 +12,61 @@ PICKLE_EXTENSION = "pkl"
 SERIALIZE_FILENAME_PREFIX = "fit_model"
 
 
+class BlendingRegressor:
+    def __init__(self, regr):
+        self.regr = regr
+        self.x_mean = None
+        self.y_mean = None
+        self.x_std = None
+        self.y_std = None
+
+    def set_regressor(self, regr):
+        self.regr = regr
+
+    def set_means_stds(self, x_mean, y_mean, x_std, y_std):
+        self.x_mean, self.y_mean, self.x_std, self.y_std = \
+            x_mean, y_mean, x_std, y_std
+
+    def get_means_stds(self):
+        return self.x_mean, self.y_mean, self.x_std, self.y_std
+
+
 class MyBlender(MyEstimatorBase, PickleMixin):
-    def __init__(self, blend_model_instance: BaseEstimator, model_name: str, params: dict):
+    def __init__(self, blending_regressor: BaseEstimator, model_name: str, params: dict):
         super().__init__(model_name, params)
-        self.regr = blend_model_instance.set_params(**params)
+        self.blend_model = BlendingRegressor(
+            blending_regressor.set_params(**params)
+        )
         self.MODELS_SERIALIZING_BASEPATH = self.path.join(self.MODELS_SERIALIZING_BASEPATH,
                                                           MACHINE_LEARNING_TECHNIQUE_NAME)
         self.SERIALIZE_FILENAME_PREFIX = SERIALIZE_FILENAME_PREFIX
 
     def get_model_instance(self):
-        return self.regr
+        return self.blend_model.regr
 
     def fit(self, X, y):
-        X, y, self.x_mean_, self.y_mean_, self.x_std_, self.y_std_ = \
-            _center_scale_xy(X, y)
+        X, y, x_mean, y_mean, x_std, y_std = _center_scale_xy(X, y)
+        self.blend_model.set_means_stds(x_mean, y_mean, x_std, y_std)
 
-        fit_model = self.regr.fit(X, y.reshape(-1))
-        self.to_pkl(self, self.gen_serialize_filepath(self.SERIALIZE_FILENAME_PREFIX, PICKLE_EXTENSION))
+        fit_model = self.blend_model.regr.fit(X, y.reshape(-1))
+        # TODO: locationに関する情報がない
+        self.to_pkl(self.blend_model, self.gen_serialize_filepath(self.SERIALIZE_FILENAME_PREFIX, PICKLE_EXTENSION))
 
         return fit_model
 
     def predict(self, X):
-        obj = self.read_pkl(self.gen_serialize_filepath(self.SERIALIZE_FILENAME_PREFIX, PICKLE_EXTENSION))
+        blend_model = self.read_pkl(self.gen_serialize_filepath(self.SERIALIZE_FILENAME_PREFIX, PICKLE_EXTENSION))
 
-        X = (X - obj.x_mean_) / obj.x_std_
-        y_pred = obj.regr.predict(X)
+        x_mean, y_mean, x_std, y_std = blend_model.get_means_stds()
+        X = (X - x_mean) / x_std
+        y_pred = blend_model.regr.predict(X)
 
-        return (y_pred * obj.y_std_) + obj.y_mean_
+        return (y_pred * y_std) + y_mean
 
     def cross_val_predict(self, X, y, cv=KFold(n_splits=NUM_FOLDING)):
         """From 'cross_val_predict' in sklearn.model_selection._validation"""
         prediction_blocks = Parallel(n_jobs=-1)(delayed(_fit_and_predict)(
-            clone(self.regr), X, y, train_index, test_index)
+            clone(self.blend_model.regr), X, y, train_index, test_index)
                                                 for train_index, test_index in cv.split(X, y))
 
         predictions = [pred_block for (pred_block, _) in prediction_blocks]
